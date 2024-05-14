633d9705c8789521df3911ff3b99fbd849789870
Heejung-KOREA <heejungs0304@gmail.com>
2024-05-14 22:36:24 +0900

    회원 API 1차
    - db 연동 오류 수정 중입니다.
    - 차후 로그인/로그아웃/회원가입 성공
      메시지 수정 예정

---
+++ supercodinPJ1/src/main/java/com/github/supercodinpj1/service/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.supercodinpj1.service;
+
+//예외 추가
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
