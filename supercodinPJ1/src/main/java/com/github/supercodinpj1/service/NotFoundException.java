050b057812f146297d068a9634ecb612f3c62e7c
0woo22 <wjsduddn1116@naver.com>
2024-05-17 03:28:20 +0900

    post+user
    post , user 병합 후 추가 구현

--- supercodinPJ1/src/main/java/com/github/supercodinpj1/service/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-//package com.github.supercodinpj1.service;
-//
-////예외 추가
-//public class NotFoundException extends RuntimeException {
-//    public NotFoundException(String message) {
-//        super(message);
-//    }
-//}
